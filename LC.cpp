19ba268a74659d1d73b326cfb9ed5d1ac80382c2
Eugen445 <1336939917@qq.com>
2021-03-14 23:36:13 +0800

    cpp

---
+++ LC.cpp
@@ -0,0 +1,156 @@
+#include<iostream>
+using namespace std;
+
+//2021_3_14
+//141. 环形链表
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if(head == nullptr || head->next == nullptr)
+//             return false;
+//             ListNode *fast = head->next;
+//             ListNode *slow = head;
+//         while(slow != fast){
+//             if(fast == nullptr || fast->next == nullptr)
+//                 return false;
+//             fast = fast->next->next;
+//             slow = slow->next;
+//         }
+//         return true;
+//     }
+// };
+
+class Solution{
+public:
+	bool hasCycle(ListNode *head) {
+		if (head == nullptr || head->next == nullptr)
+			return false;
+		ListNode *fast = head;
+		ListNode *slow = head;
+		while (fast && fast->next){
+			fast = fast->next->next;
+			slow = slow->next;
+			if (fast == slow)
+				return true;
+		}
+		return false;
+	}
+};
+
+//88. 合并两个有序数组
+class Solution {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		for (int i = 0; i != n; i++){
+			nums1[m + i] = nums2[i];
+		}
+		sort(nums1.begin(), nums1.end());
+	}
+};
+
+class Solution {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		int p1 = 0, p2 = 0;
+		//int count = 0;
+		int tmp[m + n];
+		int cur;
+
+		while (p1 <m || p2<n){
+			if (p1 == m)
+				cur = nums2[p2++];
+			else if (p2 == n)
+				cur = nums1[p1++];
+			else if (nums1[p1] < nums2[p2])
+				cur = nums1[p1++];
+			else
+				cur = nums2[p2++];
+			//tmp[count++] = cur;
+			tmp[p1 + p2 - 1] = cur;
+		}
+		for (int i = 0; i < m + n; i++)
+			nums1[i] = tmp[i];
+	}
+};
+
+class Solution {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		int p1 = m - 1, p2 = n - 1;
+		int tail = m + n - 1;
+		int cur;
+		while (p1 >= 0 || p2 >= 0){
+			if (p1 < 0)
+				cur = nums2[p2--];
+			else if (p2 < 0)
+				cur = nums1[p1--];
+			else if (nums1[p1] < nums2[p2])
+				cur = nums2[p2--];
+			else cur = nums1[p1--];
+			nums1[tail--] = cur;
+		}
+	}
+};
+
+//344.反转字符串
+class Solution {
+public:
+	void reverseString(vector<char>& s) {
+		int n = s.size();
+		int left = 0;
+		int right = n - 1;
+		int tmp;
+		while (left < right){
+			tmp = s[left];
+			s[left] = s[right];
+			s[right] = tmp;
+			left++, right--;
+		}
+	}
+};
+
+class Solution {
+public:
+	void reverseString(vector<char>& s) {
+		int n = s.size();
+		int left = 0;
+		int right = n - 1;
+		while (left < right){
+			swap(s[left], s[right]);
+			left++, right--;
+		}
+	}
+};
+
+class Solution {
+public:
+	void reverseString(vector<char>& s) {
+		int n = s.size();
+		char tmp;
+		for (int i = 0; i < n / 2; i++){
+			tmp = s[i];
+			s[i] = s[n - 1 - i];
+			s[n - 1 - i] = tmp;
+		}
+	}
+};
+
+//26. 删除排序数组中的重复项
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		if (nums.empty())
+			return 0;
+		int i = 0;
+		for (int j = 1; j < nums.size(); ++j){
+			if (nums.at(i) != nums.at(j))
+				nums.at(++i) = nums.at(j);
+		}
+		return i + 1;
+	}
+};
+
+int main()
+{
+	return 0;
+}

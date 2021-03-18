5504efd33343de6756f674c4f85f0d1ab22a28b3
Eugen445 <1336939917@qq.com>
2021-03-19 00:23:34 +0800

    LLL

--- LC.cpp
+++ LC.cpp
@@ -1,6 +1,247 @@
 #include<iostream>
 using namespace std;
 
+//2021_3_19
+
+//1603. 设计停车系统
+class ParkingSystem {
+public:
+
+	int _big, _medium, _small;
+	ParkingSystem(int big, int medium, int small) {
+		_big = big;
+		_medium = medium;
+		_small = small;
+	}
+	bool addCar(int carType) {
+
+		if (carType == 1){
+			if (_big > 0){
+				_big--;
+				return true;
+			}
+		}
+		else if (carType == 2){
+			if (_medium > 0){
+				_medium--;
+				return true;
+			}
+		}
+		else if (carType == 3){
+			if (_small > 0){
+				_small--;
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+/**
+* Your ParkingSystem object will be instantiated and called as such:
+* ParkingSystem* obj = new ParkingSystem(big, medium, small);
+* bool param_1 = obj->addCar(carType);
+*/
+
+//209. 长度最小的子数组
+// class Solution {
+// public:
+//     int minSubArrayLen(int target, vector<int>& nums) {
+//         int result = INT32_MAX;//为了第一次 len 的赋值
+//         int len = 0;
+//         int sum = 0;
+//         int size = nums.size();
+
+//         for (int i = 0; i < size; i++){
+//             sum = 0;
+//             for (int j = i; j < size; j++){
+//                 //sum += nums[i];
+//                 sum += nums[j];
+//                 if (sum >= target){
+//                     len = j - i + 1;
+//                     result = result < len ? result : len;
+//                     break;//这次循环可以结束了
+//                 }
+//             }
+//         }
+//         return result == INT32_MAX ? 0 : result;
+//     }
+// };
+
+class Solution {
+public:
+	int minSubArrayLen(int target, vector<int>& nums) {
+		int result = INT32_MAX;
+		int size = nums.size();
+		int len = 0;
+		int sum = 0;
+		int i = 0;
+
+		for (int j = 0; j < size; j++){
+			sum += nums[j];
+
+			while (sum >= target){
+				len = (j - i + 1);
+				result = result < len ? result : len;
+				sum -= nums[i++];
+			}
+		}
+		return result == INT32_MAX ? 0 : result;
+	}
+};
+
+//27. 移除元素
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         int size = nums.size();
+//         for (int i = 0; i < size; i++){
+
+//             if (nums[i] == val){
+
+//                 for (int j = i; j < size - 1; j++)
+//                     nums[j] = nums[j + 1];
+//                 // for (int j = i + 1; j < size; j++)
+//                 //     nums[j - 1] = nums[j];
+//                 i--;
+//                 size--;
+//             }
+//         }
+//         return size;
+//     }
+// };
+
+class Solution {
+public:
+	int removeElement(vector<int>& nums, int val) {
+		int fast = 0, slow = 0;
+		int size = nums.size();
+
+		while (fast < size){
+
+			if (nums[fast] == val)
+				++fast;
+			else
+				nums[slow++] = nums[fast++];
+		}
+		return slow;
+	}
+};
+
+
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//             int n = nums.size();
+//             int count = 0;
+//             for (int i = 0; i < n; i++){
+//                 if (nums[i] != val)
+//                     nums[count++] = nums[i];
+//             }
+//             return count;
+//     }
+// };
+
+//35. 搜索插入位置
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int left = 0, right = nums.size() - 1;
+//         while (left <= right){
+//             int mid = left + right;
+//             if (nums[mid] < target)
+//                 left = mid + 1;
+//             else if (nums[mid] > target)
+//                 right = mid - 1;
+//             else return mid;
+//         }
+//         return left;//return right + 1
+//     }
+// };
+
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int left = 0;
+//         int right = n;
+//         while (left < right){
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] > target)
+//                 right = mid;
+//             else if (nums[mid] < target)
+//                 left = mid + 1;
+//             else return mid;
+//         }
+//         return right;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int left = 0;
+//         int right = n - 1;//[1,3,5,6],7 这种情况会出错
+//         while (left < right){
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] > target)
+//                 right = mid;
+//             else if (nums[mid] < target)
+//                 left = mid + 1;
+//             else return mid;
+//         }
+//         return right;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         for (int i = 0; i < nums.size(); i++){
+//             if (nums[i] >= target)
+//                 return i;
+//         }
+//         return nums.size();
+//     }
+// };
+
+//92. 反转链表 II
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
+class Solution {
+private:
+	void reverse(ListNode *head){
+		ListNode *pre = nullptr;
+		ListNode *cur = head;
+		while (cur != nullptr){
+			ListNode *next = cur->next;
+			cur->next = pre;
+			pre = cur;
+			cur = next;
+		}
+
+	}
+public:
+	ListNode* reverseBetween(ListNode* head, int left, int right) {
+		listNode *dummyNode = new ListNode(-1);
+		dummyNode->next = head;
+
+		ListNode *pre = dummyNode;
+		for (int i = 0; i < left - 1; i++)
+			pre = pre->next;
+
+	}
+};
+
 //2021_3_15
 //925. 长按键入
 class Solution {

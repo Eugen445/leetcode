c43d28bfc729abd916f97ff11d3877a9b37aad54
Eugen445 <1336939917@qq.com>
2021-04-12 20:25:32 +0800

    2021_4_12

--- LC.cpp
+++ LC.cpp
@@ -1,6 +1,52 @@
 #include<iostream>
 using namespace std;
 
+//2021_4_12
+//179. 最大数
+class Solution {
+public:
+	string largestNumber(vector<int>& nums) {
+		vector<string> strNums(nums.size());
+		for (auto i = 0; i < nums.size(); ++i) {
+			strNums[i] = to_string(nums[i]);
+		}
+
+		sort(strNums.begin(), strNums.end(), compare);
+		string ans;
+		for (const auto& strNum : strNums) {
+			ans += strNum;
+		}
+
+		if (ans.length() > 0 && ans[0] == '0') {
+			return "0";
+		}
+
+		return ans;
+	}
+
+private:
+	static bool compare(const string& str1, const string& str2)
+	{
+		int length1 = str1.size();
+		int length2 = str2.size();
+
+		int length = min(length1, length2);
+		auto i = 0;
+		while (i < length) {
+			if (str1[i] != str2[i]) {
+				return str1[i] > str2[i];
+			}
+
+			++i;
+		}
+
+		string compare1 = str1 + str2;
+		string compare2 = str2 + str1;
+
+		return compare1 > compare2;
+	}
+};
+
 //2021_4_11
 
 //34. 在排序数组中查找元素的第一个和最后一个位置

6da6ed6b801f5590d41e8bb89326169c025abb2a
Eugen445 <1336939917@qq.com>
2021-04-29 22:49:12 +0800

    2021_4_29

--- LC.cpp
+++ LC.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
 
+//2021_4_29
+//403. 青蛙过河
+class Solution {
+public:
+	bool canCross(vector<int>& stones) {
+		int n = stones.size();
+		vector<vector<int>> dp(n, vector<int>(n));
+		dp[0][0] = true;
+		for (int i = 1; i < n; ++i) {
+			if (stones[i] - stones[i - 1] > i) {
+				return false;
+			}
+		}
+		for (int i = 1; i < n; ++i) {
+			for (int j = i - 1; j >= 0; --j) {
+				int k = stones[i] - stones[j];
+				if (k > j + 1) {
+					break;
+				}
+				dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1];
+				if (i == n - 1 && dp[i][k]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+};
 //2021_4_22
 //363. 矩形区域不超过 K 的最大数值和
 class Solution {

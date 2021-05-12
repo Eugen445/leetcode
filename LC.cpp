73ec0fa9138c6dc0b11a361b714b5026794fd477
Eugen445 <1336939917@qq.com>
2021-05-12 22:49:51 +0800

    2021_5_12

--- LC.cpp
+++ LC.cpp
@@ -1,6 +1,70 @@
 #include<iostream>
 using namespace std;
 
+//2021_5_12
+//76. 最小覆盖子串
+class Solution {
+public:
+	string minWindow(string s, string t) {
+		vector<int> map(128);
+		int left = 0, right = 0;
+		int need = t.size();
+		int minStart = 0;
+		int minLen = INT_MAX;
+		for (char ch : t) ++map[ch];
+		while (right < s.size()) {
+			if (map[s[right]] > 0) --need;
+			--map[s[right]];
+			++right;
+			while (need == 0) {
+				if (right - left < minLen) {
+					minStart = left;
+					minLen = right - left;
+				}
+				if (++map[s[left]] > 0) ++need;
+				++left;
+			}
+		}
+		if (minLen != INT_MAX) return s.substr(minStart, minLen);
+		return "";
+	}
+};
+
+//103. 二叉树的锯齿形层序遍历
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+*     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+*     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+* };
+*/
+class Solution {
+public:
+	vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+		vector<vector<int>> res;
+		queue<TreeNode*> que;
+		if (root != nullptr) que.push(root);
+		bool left = true;
+		while (!que.empty()) {
+			int size = que.size();
+			vector<int> vec;
+			for (int i = 0; i < size; ++i) {
+				TreeNode *cur = que.front(); que.pop();
+				vec.push_back(cur->val);
+				if (cur->left) que.push(cur->left);
+				if (cur->right) que.push(cur->right);
+			}
+			if (!left) reverse(vec.begin(), vec.end());
+			left = !left;
+			res.push_back(vec);
+		}
+		return res;
+	}
+};
 //2021_5_11
 //剑指 Offer 59 - I. 滑动窗口的最大值
 class Solution {
